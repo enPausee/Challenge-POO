a257407044314fc4790c43cc12f4530853cc966e
Matthias Lagrabe <matthias.lagrabe@gmail.com>
2021-11-05 17:36:13 +0100

    added type to method

--- Speedometer.php
+++ Speedometer.php
@@ -2,15 +2,15 @@
 
 class Speedometer
 {
-    public static function convertKmToMiles($km)
+    public static function convertKmToMiles($km): float
     {
         return $km * 0.621371;
     }
 
-    public static function convertMilesToKm($miles)
+    public static function convertMilesToKm($miles): float
     {
         return $miles * 1.60934;
     }
 }
 
-echo Speedometer::convertKmToMiles(10) . PHP_EOL;
+echo Speedometer::convertMilesToKm(10) . PHP_EOL;
